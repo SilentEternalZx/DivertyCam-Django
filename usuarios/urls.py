c3dd93bd9a93eafdd93d01f65a72a298dfb6d56c
Julián <julianrpm18@gmail.com>
2025-02-19 06:48:42 -0500

    cambios nuevos

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path("login", views.login_view, name="login"),
+    path("", views.index, name="index"),
+    path("login/", views.login_view, name="login"),
     path("register", views.register_view, name="register"),
     path("logout", views.logout_view, name="logout"),
     path("permisos", views.obtener_permisos_usuario, name="obtener_permisos"),
     path("asignar-rol/<int:usuario_id>/<int:rol_id>", views.asignar_rol_usuario, name="asignar_rol"),
-    path("", views.index, name="index")
+    path("password_reset/", auth_views.PasswordResetView.as_view(), name="password_reset"),
+    path("password_reset/done/", auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path("reset/done/", auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+   
+  
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
+
 from .models import User, Rol, Permiso
 
 def index(request):
@@ -38,6 +39,7 @@ def register_view(request):
         confirmacion = request.POST.get("confirmacion")
 
         if contraseña != confirmacion:
+            
             mensaje = "Las contraseñas no coinciden"
         elif User.objects.filter(username=nombre_usuario).exists():
             mensaje = "El nombre de usuario ya está en uso"
@@ -68,3 +70,11 @@ def asignar_rol_usuario(request, usuario_id, rol_id):
         return JsonResponse({"mensaje": "Usuario no encontrado"}, status=404)
     except Rol.DoesNotExist:
         return JsonResponse({"mensaje": "Rol no encontrado"}, status=404)
+
+
+def password_recovery(request):
+    
+    if request.method=="POST":
+        email=request.POST["email"]
+        
+    return render(request,"registration/password_reset_form.html")
