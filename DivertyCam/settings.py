b07589765926a827a0cf75acdc87630275202de0
Norus55 <garcessamuel6550@gmail.com>
2025-02-25 07:16:49 -0500

    new changes

--- DivertyCam/settings.py
+++ DivertyCam/settings.py
@@ -36,7 +36,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'jazzmin',
     'usuarios',
     'jazzmin',
     'django.contrib.admin',
--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
-
+from . import admin
 urlpatterns = [
     path("", views.index, name="index"),
     path("login/", views.login_view, name="login"),
