30cfe4885213018e8279078d5808130e30f9f258
Julián <julianrpm18@gmail.com>
2025-03-24 18:53:30 -0500

    Fusión 24 de marzo

--- DivertyCam/settings.py
+++ DivertyCam/settings.py
@@ -55,7 +55,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    
     'django.contrib.staticfiles',
     'crispy_forms',
     'crispy_bootstrap5',
